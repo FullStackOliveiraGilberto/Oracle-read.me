1e5ad5d92d6c4123dce3767dcd2f8792cd0cc557
Gilberto Oliveira <skwcontadores@gmail.com>
2019-04-04 11:17:39 -0300

    EXCEPTION

--- Oracle_Exceptions.sql
+++ Oracle_Exceptions.sql
@@ -17,20 +17,29 @@ declare
     ERR_TRATA_ERRO            EXCEPTION;
     ERR_PREVISTO              EXCEPTION;
     ERR_NAO_PREVISTO          EXCEPTION;
-       
+
+begin       
      begin 
        P_IND_ERRO :='0';  
-       select salary into v_time_i_one  from TEMP_TESTE where my_name = 'JOEJWLBDM'; --JOEJWZLBDM
+       select salary into v_time_i_one  from TEMP_TESTE where my_name = 'JOEJWZLBDM'; --JOEJWZLBDM
        
        select salary into v_time_i_one  from TEMP_TESTE where my_name = 'QKCLVVYHGD'; --QKCLVVYHGD
          
-       dbms_output.Put_line('achou' );
+       dbms_output.Put_line('Achou' );
         
       EXCEPTION
-      WHEN NO_DATA_FOUND THEN
-        P_IND_ERRO := '1';
-         dbms_output.Put_line('Não encontrado!!' );
-        RAISE ERR_TRATA_ERRO;
-        
+          WHEN NO_DATA_FOUND THEN
+            P_IND_ERRO := '1';
+             dbms_output.Put_line('Não encontrado!!' );
+            RAISE ERR_TRATA_ERRO;
+      END;
+  
+EXCEPTION
+WHEN ERR_TRATA_ERRO THEN
+    dbms_output.Put_line('executa trata erro!!' );
+END;
+  
+  
     
-  END;
+    
+    
